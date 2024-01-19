9c28a74585a53e7fb7bb27bd1b5bc447f48a1218
root <root@DESKTOP-34M0MOJ>
2024-01-19 10:28:40 +0530

    commit 2

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, This is vijay!');
+  res.send('Hello, AWS Elastic Beanstalk!');
 });
 
 // Export the app object
